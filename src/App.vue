2609f2ef9de8022694588a22a7c95b9da417f3f6
Anton Karpenko <karpolan@karpolan.com>
2019-05-06 07:28:48 +0300

    Header and Footerl added

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,45 @@
 <template>
 	<div id="app">
-		<div id="nav">
-			<router-link to="/">Home</router-link> | <router-link to="/contact">Contact</router-link> |
-			<router-link to="/about">About</router-link>
-		</div>
+		<Header />
+
 		<router-view />
+		
+		<Footer />
 	</div>
 </template>
 
+<script>
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+
+export default {
+	name: 'App',
+	components: {
+		Header,
+		Footer,
+	},
+};
+</script>
+
+
 <style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+    monospace;
+}
+
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-}
-#nav {
-	padding: 30px;
+	text-align: left;
 }
 
-#nav a {
-	font-weight: bold;
-	color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-	color: #42b983;
-}
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,90 @@
+<template>
+	<footer class="footer">
+		<div class="container small pt-3 bg-light">
+			<div class="row">
+
+				<div class="col-md-3 d-none d-md-block">
+					<ul class="list-unstyled">
+						<li><a target="_blank" rel="noopener noreferrer" href="https://software.karpolan.com" :title="'Software created by ' + name">Software</a></li>
+						<li><router-link to="/" :title="'Websites created by ' + name">Websites</router-link></li>
+					<!--	
+						<li><a target="_blank" rel="noopener noreferrer" href="https://services.karpolan.com" :title="'Services created by ' + name">Services</a></li>
+					-->	
+					</ul>
+				</div>
+
+				<div class="col-md-3 d-none d-md-block">
+					<ul class="list-unstyled">
+						<li><a target="_blank" rel="noopener noreferrer" href="https://book-product.karpolan.com" :title="'Books by ' + name">Books</a></li>
+					<!--	
+						<li><a target="_blank" rel="noopener noreferrer" href="https://karpolan.blogspot.com" :title="'Articles by ' + name">Articles</a></li>
+					-->	
+						<li><a target="_blank" rel="noopener noreferrer" href="https://blog.karpolan.com" :title="'Blog by ' + name">Blog</a></li>
+					</ul>
+				</div>
+
+				<div class="social col-md-6 text-center text-md-right">
+
+					<ul class="list-unstyled list-inline social-icons d-none d-md-block">
+						<SocialIcon :link="linkedin" icon="linkedin" :title="name + ' on LinkedIn'" />
+						<SocialIcon :link="facebook" icon="facebook" :title="name + ' on Facebook'" />
+						<SocialIcon :link="twitter" icon="twitter" :title="name + ' on Twitter'" />
+						<SocialIcon :link="github" icon="github" :title="name + ' on GitHub'" />
+					</ul>
+
+					<div class="copyright pb-3 d-none d-sm-block">
+						Copyright &copy; 1994-{{new Date().getFullYear() + ' '}}
+						<a target="_blank" rel="noopener noreferrer" href="https://karpolan.com/">KARPOLAN</a>
+					</div>
+
+				</div>
+
+			</div>
+		</div>
+	</footer>	
+</template>
+
+<script>
+import SocialIcon from './SocialIcon';
+import '@/assets/social-colors.css'; // Colors for social icons
+
+// Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+	faLinkedin,
+	faFacebook,
+	faTwitter,
+	faGithub,
+} from '@fortawesome/free-brands-svg-icons';
+library.add(
+	faLinkedin,
+	faFacebook,
+	faTwitter,
+	faGithub,
+);
+
+// Application Data
+import {
+	name,
+	linkedin,
+	facebook,
+	twitter,
+	github,
+} from '@/data/consts';
+
+export default {
+	name: 'Footer',
+
+	data: () => ({
+		name,
+		linkedin,
+		facebook,
+		twitter,
+		github,
+	}),
+
+	components: {
+		SocialIcon,
+	},
+};
+</script>
