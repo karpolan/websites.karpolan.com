6a0bd5093337dded62e9ee3e5314a46ad8de208e
Anton Karpenko <karpolan@karpolan.com>
2020-08-29 19:42:10 +0200

    feat: router supports /index.html as aliases

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,8 +6,8 @@
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-          <b-nav-item to="/contact/index.html">Contact</b-nav-item>
-          <b-nav-item to="/about/index.html">About</b-nav-item>
+          <b-nav-item to="/contact/">Contact</b-nav-item>
+          <b-nav-item to="/about/">About</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
